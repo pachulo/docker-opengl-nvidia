0d3c797392864c83c4f459e7444cbe3183a64a3e
pachulo <segarrra@gmail.com>
2015-12-12 23:15:52 +0100

    Fix the Docker version guessing in the run.sh script.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" = "$1"; }
-docker_version=$(docker version | grep 'Client version' | awk '{split($0,a,":"); print a[2]}' | tr -d ' ')
+docker_version=$(docker version --format '{{.Client.Version}}')
 # Docker 1.3.0 or later is required for --device
 if ! version_gt "${docker_version}" "1.2.0"; then
 	echo "Docker version 1.3.0 or greater is required"
